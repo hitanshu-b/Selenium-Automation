9d00d2011fb904504b43033fbc77d474f163f044
hitanshu-b <hitanshubagde196@gmail.com>
2024-03-23 11:14:26 +0530

    Initial Commit

--- src/test/java/org/example/Selenium2/Selenium17.java
+++ src/test/java/org/example/Selenium2/Selenium17.java
@@ -1,6 +1,37 @@
 package org.example.Selenium2;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class Selenium17 {
-    // Selenium waits
-    // selenium axes
+    // CSS Selectors
+
+    @Test
+    public void testCssSelector() throws InterruptedException {
+
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.ebay.com/b/PC-Desktops-All-In-One-Computers/179/bn_661752");
+
+        Thread.sleep(3000);
+
+        WebElement element = driver.findElement(By.cssSelector("input[type='text']"));
+        element.sendKeys("Mac");
+
+        WebElement element1 = driver.findElement(By.cssSelector("input[type='submit']"));
+        element1.click();
+
+        Thread.sleep(2000);
+
+        List<WebElement> elementList = driver.findElements(By.cssSelector("span[role='heading']"));
+        for(int i=0; i<elementList.size() ;i++){
+            System.out.println(elementList.get(i).getText());
+        }
+
+        driver.quit();
+    }
 }
