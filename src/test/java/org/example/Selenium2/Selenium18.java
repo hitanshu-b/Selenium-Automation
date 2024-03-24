9f2e2df49288618643605281ef181e15c8defd13
hitanshu-b <hitanshubagde196@gmail.com>
2024-03-24 10:59:12 +0530

    Initial Commit

---
+++ src/test/java/org/example/Selenium2/Selenium18.java
@@ -0,0 +1,12 @@
+package org.example.Selenium2;
+
+import io.qameta.allure.Description;
+import org.testng.annotations.Test;
+
+public class Selenium18 {
+    @Test
+    @Description("Test selenium waits")
+    public void testDemo(){
+
+    }
+}
